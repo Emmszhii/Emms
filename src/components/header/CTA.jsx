c32eaab974934028ca4dda3df6b6c01d6f8032e3
Emmszhii <emmanuelmanalo707@gmail.com>
2023-02-17 20:41:54 +0800

    Update cv and photo

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CV from '../../assets/cv.pdf';
+import CV from '../../assets/EmmsCV.pdf';
 
 const CTA = () => {
   return (
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CTA from './CTA';
-import ME from '../../assets/me.png';
+import ME from '../../assets/Emms.png';
 import HeaderSocials from './HeaderSocials';
 import './header.css';
 
